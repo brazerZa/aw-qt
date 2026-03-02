cc0c3522323b688858466f5bba87abe9652cfdeb
Hennie Hamman <hennieh@gmail.com>
2026-03-02 21:21:07 +0200

    fix: update all Malachi logos

--- aw_qt/trayicon.py
+++ aw_qt/trayicon.py
@@ -82,7 +82,7 @@ class TrayIcon(QSystemTrayIcon):
     ) -> None:
         QSystemTrayIcon.__init__(self, icon, parent)
         self._parent = parent  # QSystemTrayIcon also tries to save parent info but it screws up the type info
-        self.setToolTip("ActivityWatch" + (" (testing)" if testing else ""))
+        self.setToolTip("Malachi ActivityWatch" + (" (testing)" if testing else ""))
 
         self.manager = manager
         self.testing = testing
@@ -138,9 +138,9 @@ class TrayIcon(QSystemTrayIcon):
         # Seems to be in agreement with: https://github.com/OtterBrowser/otter-browser/issues/1313
         #   "it seems that the bug is also triggered when creating a QIcon with an invalid path"
         if exitIcon.availableSizes():
-            menu.addAction(exitIcon, "Quit ActivityWatch", lambda: exit(self.manager))
+            menu.addAction(exitIcon, "Quit Malachi ActivityWatch", lambda: exit(self.manager))
         else:
-            menu.addAction("Quit ActivityWatch", lambda: exit(self.manager))
+            menu.addAction("Quit Malachi ActivityWatch", lambda: exit(self.manager))
 
         self.setContextMenu(menu)
 
